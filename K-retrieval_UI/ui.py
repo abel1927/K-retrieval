fc16c182afded7c3713d1ac38735c4b7e2f726c5
Abel Molina Sánchez <80501536+abel1927@users.noreply.github.com>
2021-12-10 12:45:01 -0500

    Create ui.py
    Inicial commit

---
+++ K-retrieval_UI/ui.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def title():
+    st.markdown(
+    "<h1 style='text-align: center; '>K-retrieval21</h1>",
+    unsafe_allow_html=True,)
+
+def load_source():
+    search = st.text_input('Enter the path of information')
+    if search:
+        loads = True
+
+def search_engine():
+    pass
+
+loads = False
+
+if __name__ == '__main__':
+    title()
+    if loads:
+        load_source()
+    else:  
+        search_engine()
